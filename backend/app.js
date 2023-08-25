bdb8ef8bf9b2e35de4c2907717d741f2487d8956
Quentin Nicot <q.nicot@icloud.com>
2023-08-25 19:29:51 +0200

    connection a la base de donnée reussie

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,53 @@
-const express = require ('express');
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://quentin:15171517@cluster0.1xy4ice.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.post('/api/books', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
+
+app.get('/api/books', (req, res, next) => {
+    const book = [
+    {
+        userId :"Blio",
+        title :"Harry Potter à l\'écoles des sorciers",
+        author : "J.K Rolling",
+        imgUrl : " ",
+        year : 1997,
+        genre : "roman jeunesse",
+        ratings : 5,
+        averageRating : 4,
+    },
+    {
+        userId :"Blio",
+        title :"Harry Potter et la chambres des secrets",
+        author : "J.K Rolling",
+        imgUrl : " ",
+        year : 1998,
+        genre : "roman jeunesse",
+        ratings : 5,
+        averageRating : 4,
+    },
+    ];
+    res.status(200).json(book);
+});
 module.exports = app;
