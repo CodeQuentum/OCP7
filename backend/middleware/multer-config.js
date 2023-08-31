5c4cdbb6d4d2f2e65c4766145900df93376e41a3
Quentin Nicot <q.nicot@icloud.com>
2023-08-31 15:34:11 +0200

    Ajout de Sharp pour optimiser les images mise en ligne

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const sharp = require('sharp'); 
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -6,15 +7,41 @@ const MIME_TYPES = {
   'image/png': 'png'
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});
+const storage = multer.memoryStorage(); 
 
-module.exports = multer({storage: storage}).single('image');
+const fileFilter = (req, file, callback) => {
+  const isValidMimeType = MIME_TYPES[file.mimetype];
+  if (isValidMimeType) {
+    callback(null, true);
+  } else {
+    callback(new Error('Invalid file type'));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter
+}).single('image');
+
+module.exports = (req, res, next) => {
+  upload(req, res, (error) => {
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+    
+    if (req.file) {
+      sharp(req.file.buffer)
+        .resize(400, 600)
+        .toFormat('jpeg')
+        .jpeg({ quality: 70 })
+        .toFile(`images/${req.file.filename}`, (err) => {
+          if (err) {
+            return res.status(500).json({ error: err.message });
+          }
+          next();
+        });
+    } else {
+      next();
+    }
+  });
+};
