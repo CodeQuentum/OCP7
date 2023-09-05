9bcde7eb007f5b211559c74f83e4fee7b9a1def7
Quentin Nicot <q.nicot@icloud.com>
2023-09-05 14:49:26 +0200

    Cette fois c'est la bonne ! tout fonctionne !

--- backend/middleware/sharp-config.js
+++ backend/middleware/sharp-config.js
@@ -1,23 +1,34 @@
 const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
 
 function sharpMiddleware(req, res, next) {
   if (!req.file) {
     return next();
   }
-
-  sharp(req.file.path)
+  
+  sharp(req.file.buffer)
     .resize(400, 600)
     .jpeg({ quality: 80 })
     .toBuffer()
     .then((buffer) => {
 
-      req.file.buffer = buffer;
+      const uniqueFileName = generateUniqueFileName();
+      const imagePath = path.join(__dirname, '..', 'images', uniqueFileName);
+      fs.writeFileSync(imagePath, buffer);
+      req.file.filename = uniqueFileName;
       next();
     })
     .catch((err) => {
       console.error('Erreur lors du redimensionnement de l\'image :', err);
       return res.status(500).json({ error: 'Erreur lors du redimensionnement de l\'image' });
     });
+}
+
+function generateUniqueFileName() {
+  const timestamp = Date.now();
+  const randomString = Math.random().toString(36).substring(2, 15);
+  return `image_${timestamp}_${randomString}.jpeg`;
 }
 
 module.exports = sharpMiddleware;
--- backend/routes/Book.js
+++ backend/routes/Book.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
-const sharpMiddleware = require('../middleware/sharp-config');
+const Sharp = require('../middleware/sharp-config');
 const bookCtrl = require('../controllers/Book');
 
-router.post('/', auth, multer, sharpMiddleware, bookCtrl.createBook);
+router.post('/', auth, multer, Sharp, bookCtrl.createBook);
 router.post('/:id/rating', auth, bookCtrl.rateBook);
-router.put('/:id', auth, multer, sharpMiddleware, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, Sharp, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/bestrating', bookCtrl.getBestRatedBooks);
 router.get('/:id', bookCtrl.getOneBook);
